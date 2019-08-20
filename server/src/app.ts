40f149feb13751bc5528a8e73f82496115fcbe5f
Sindre Slungaard <slungaards@gmail.com>
2019-08-20 22:35:07 +0200

    connect websockets, add types for mongoose

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import { createServer } from "http"
 import logger from "./utils/logger"
 import api from "./api"
+import * as server from "./net/server"
 
 export const bootstrap = async () => {
 
     logger.info("Starting up...")
 
     const web = createServer(api)
+
+    server.connect(web)
 
     web.listen(process.env.PORT || 3000, () => {
         logger.info(`Listening on port ${process.env.PORT || 3000}`)
