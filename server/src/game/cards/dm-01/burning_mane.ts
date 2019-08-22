71d961471ff57ff2be07e5491a006b8a4cf93d64
Sindre Slungaard <slungaards@gmail.com>
2019-08-22 16:56:29 +0200

    wip events

--- server/src/game/cards/dm-01/burning_mane.ts
+++ server/src/game/cards/dm-01/burning_mane.ts
@@ -1,11 +1,19 @@
-import { creature, civilization } from "../types"
+import { creature, civilization, family } from "../types"
+import { before } from "../../match"
 
 const burningMane: creature = {
-    
+
+    name: "Burning Mane",
+    civilization: civilization.NATURE,
+    family: family.BEAST_FOLK,
     manaCost: 2,
     manaRequirement: [civilization.NATURE],
 
     setup() {
+        
+        before('turn-start', (event, next) => {
+            
+        })
 
     }
 
---
+++ server/src/game/match.ts
@@ -0,0 +1,1 @@
+export const before = (event: string, callback: Function) => {}
