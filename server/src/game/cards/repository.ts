6f0f8c02f07ae79689e4afe6e09b97fb3e836257
Sindre Slungaard <slungaards@gmail.com>
2019-08-28 23:21:44 +0200

    load cards to memory

--- server/src/game/cards/repository.ts
+++ server/src/game/cards/repository.ts
@@ -4,8 +4,30 @@ import { join } from "path"
 
 const cardDatabase: {[key: string]: card} = {}
 
-export const load = () => {
+export const load = async () => {
 
-    let files = readdirSync(__dirname).filter(f => statSync(join(__dirname, f)).isDirectory())
+    let dirs = readdirSync(__dirname).filter(f => statSync(join(__dirname, f)).isDirectory() && statSync(join(__dirname, f)))
+
+    for(let set of dirs) {
+
+        if(set === "types") {
+            continue
+        }
+
+        let files = readdirSync(__dirname + "/" + set).filter(f => statSync(join(__dirname + "/" + set, f)).isFile())
+
+        for(let file of files) {
+            
+            if(file.includes(".js.map")) {
+                continue
+            }
+
+            let card = await import(__dirname + "/" + set + "/" + file)
+
+            cardDatabase[card.default.uid] = card.default
+            
+        } 
+
+    }
 
 }
--- server/src/game/cards/types/card.ts
+++ server/src/game/cards/types/card.ts
@@ -1,6 +1,7 @@
 import { civilization, family } from "./index"
 
 export default interface card {
+    uid: string,
     name: string,
     civilization: civilization,
     family: family,
