1246b97be77429017d2e64c8eb240deed13dd472
Sindre Slungaard <slungaards@gmail.com>
2019-09-11 21:23:53 +0200

    card locations

---
+++ server/src/game/cards/types/location.ts
@@ -0,0 +1,11 @@
+enum Location {
+    DECK = "deck",
+    HAND = "hand",
+    SHIELDZONE = "shieldzone",
+    MANAZONE = "manazone",
+    GRAVEYARD = "graveyard",
+    BATTLEZONE = "battlezone",
+    HODDENZONE = "hiddenzone"
+}
+
+export default Location
