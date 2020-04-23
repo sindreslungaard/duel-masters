c02d646938edccaae06e0ee2a28d93e6177c083c
Sindre Slungaard <slungaards@gmail.com>
2020-04-23 11:16:59 +0200

    ws message parser

--- server/parser.go
+++ server/parser.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"strconv"
+)
+
+// Parse handles a websocket message
+func Parse(data string) {
+
+	runes := []rune(data)
+
+	header, err := strconv.Atoi(string(runes[0:4]))
+
+	if err != nil {
+		return
+	}
+
+	switch header {
+
+	// Login
+	case 1000:
+		return
+
+	// Register
+	case 1001:
+		return
+
+	default:
+		return
+
+	}
+
+}
