8d4cb8593a37f19a5381344eb21a23c8f0b8f0b1
Sindre Slungaard <slungaards@gmail.com>
2020-05-22 22:45:13 +0200

    improve search helpers

--- game/match/helpers.go
+++ game/match/helpers.go
@@ -1,6 +1,6 @@
 package match
 
-// AssertCardsIn returns true or false based on if the specified card ids are present in the source []Card
+// AssertCardsIn returns true or false based on if the specified card ids are present in the source []*Card
 func AssertCardsIn(src []*Card, test ...string) bool {
 
 	for _, toTest := range test {
@@ -23,30 +23,20 @@ func AssertCardsIn(src []*Card, test ...string) bool {
 
 }
 
-// SearchDeckForCnd prompts the user to select n cards from their deck that matches the given condition
-func SearchDeckForCnd(p *Player, m *Match, condition string, text string, min int, max int, cancellable bool) []*Card {
+// Search prompts the user to select n cards from the specified container
+func Search(p *Player, m *Match, containerName string, text string, min int, max int, cancellable bool) []*Card {
 
 	result := make([]*Card, 0)
 
-	deck, err := p.Container(DECK)
+	cards, err := p.Container(containerName)
 
-	if err != nil || len(deck) < 1 {
-		return result
-	}
-
-	cards := make([]*Card, 0)
-
-	for _, c := range deck {
-		if c.HasCondition(condition) {
-			cards = append(cards, c)
-		}
-	}
-
-	if len(cards) < 1 {
+	if err != nil || len(cards) < 1 {
 		return result
 	}
 
 	m.NewAction(p, cards, min, max, text, true)
+
+	defer m.CloseAction(p)
 
 	for {
 
@@ -63,7 +53,7 @@ func SearchDeckForCnd(p *Player, m *Match, condition string, text string, min in
 
 		for _, c := range action.Cards {
 
-			selectedCard, err := p.GetCard(c, DECK)
+			selectedCard, err := p.GetCard(c, containerName)
 
 			if err != nil {
 				continue
@@ -80,21 +70,21 @@ func SearchDeckForCnd(p *Player, m *Match, condition string, text string, min in
 
 }
 
-// SearchDeckForFamily prompts the user to select n cards from their deck that matches the given family
-func SearchDeckForFamily(p *Player, m *Match, family string, text string, min int, max int, cancellable bool) []*Card {
+// SearchForCnd prompts the user to select n cards from the specified container that matches the given condition
+func SearchForCnd(p *Player, m *Match, containerName string, condition string, text string, min int, max int, cancellable bool) []*Card {
 
 	result := make([]*Card, 0)
 
-	deck, err := p.Container(DECK)
+	container, err := p.Container(containerName)
 
-	if err != nil || len(deck) < 1 {
+	if err != nil || len(container) < 1 {
 		return result
 	}
 
 	cards := make([]*Card, 0)
 
-	for _, c := range deck {
-		if c.Family == family {
+	for _, c := range container {
+		if c.HasCondition(condition) {
 			cards = append(cards, c)
 		}
 	}
@@ -104,6 +94,8 @@ func SearchDeckForFamily(p *Player, m *Match, family string, text string, min in
 	}
 
 	m.NewAction(p, cards, min, max, text, true)
+
+	defer m.CloseAction(p)
 
 	for {
 
@@ -118,8 +110,74 @@ func SearchDeckForFamily(p *Player, m *Match, family string, text string, min in
 			continue
 		}
 
-		for _, c := range cards {
-			result = append(result, c)
+		for _, c := range action.Cards {
+
+			selectedCard, err := p.GetCard(c, containerName)
+
+			if err != nil {
+				continue
+			}
+
+			result = append(result, selectedCard)
+		}
+
+		break
+
+	}
+
+	return result
+
+}
+
+// SearchForFamily prompts the user to select n cards from the specified container that matches the given family
+func SearchForFamily(p *Player, m *Match, containerName string, family string, text string, min int, max int, cancellable bool) []*Card {
+
+	result := make([]*Card, 0)
+
+	container, err := p.Container(containerName)
+
+	if err != nil || len(container) < 1 {
+		return result
+	}
+
+	cards := make([]*Card, 0)
+
+	for _, c := range container {
+		if c.Family == family {
+			cards = append(cards, c)
+		}
+	}
+
+	if len(cards) < 1 {
+		return result
+	}
+
+	m.NewAction(p, cards, min, max, text, true)
+
+	defer m.CloseAction(p)
+
+	for {
+
+		action := <-p.Action
+
+		if cancellable && action.Cancel {
+			break
+		}
+
+		if len(action.Cards) < min || len(action.Cards) > max || !AssertCardsIn(cards, action.Cards...) {
+			m.ActionWarning(p, "The cards you selected does not meet the requirements")
+			continue
+		}
+
+		for _, c := range action.Cards {
+
+			selectedCard, err := p.GetCard(c, containerName)
+
+			if err != nil {
+				continue
+			}
+
+			result = append(result, selectedCard)
 		}
 
 		break
