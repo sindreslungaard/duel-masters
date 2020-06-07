ca4417d9e3ea4036e698fc4d2b93d595999a436b
Sindre Slungaard <slungaards@gmail.com>
2020-06-07 22:13:11 +0200

    New card: Spiral Grass

---
+++ game/cards/dm02/starlight_tree.go
@@ -0,0 +1,32 @@
+package dm02
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+)
+
+// SpiralGrass ...
+func SpiralGrass(c *match.Card) {
+
+	c.Name = "Spiral Grass"
+	c.Power = 2500
+	c.Civ = civ.Light
+	c.Family = family.StarlightTree
+	c.ManaCost = 4
+	c.ManaRequirement = []string{civ.Light}
+
+	c.Use(fx.Creature, fx.Blocker, func(card *match.Card, ctx *match.Context) {
+
+		if event, ok := ctx.Event.(*match.CreatureDestroyed); ok {
+
+			if event.Source == card && event.Blocked {
+				card.Tapped = false
+			}
+
+		}
+
+	})
+
+}
