21783a1aa7f6b3f2d71b61465084a301949dba24
Severin Kaderli <severin@kaderli.dev>
2020-06-10 21:48:17 +0200

    Add dialog for showing cards to a player.
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/buttons/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="$emit('click')" class="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  display: inline-block;
+  background-color: #7289da;
+  color: #e3e3e5;
+  font-size: 14px;
+  line-height: 20px;
+  padding: 10px;
+  border-radius: 4px;
+  transition: 0.1s;
+  text-align: center !important;
+  user-select: none;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background-color: #677bc4;
+  }
+}
+</style>
---
+++ webapp/src/components/dialogs/CardShowDialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-show-dialog">
+    {{ message }}
+    <div class="cards">
+      <img
+        v-for="card in cards"
+        :key="card"
+        :src="`/assets/cards/all/${card}.jpg`"
+      />
+    </div>
+    <Button text="Close" @click="$emit('close')" />
+  </div>
+</template>
+
+<script>
+import Button from "../buttons/Button";
+
+export default {
+  name: "CardShowDialog",
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    cards: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Button
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card-show-dialog {
+  text-align: center;
+
+  .cards {
+    display: flex;
+
+    img {
+      display: block;
+      margin: 20px auto;
+      padding: 0 10px;
+      flex: 1 1 0;
+      width: 0;
+
+      &:first-child {
+        padding-left: 0;
+      }
+
+      &:last-child {
+        padding-right: 0;
+      }
+    }
+  }
+}
+</style>
