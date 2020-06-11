a374a95fe967640d2834b01b6045cb6d1f4df331
Severin Kaderli <severin@kaderli.dev>
2020-06-11 14:11:48 +0200

    Add components for links
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/links/ExternalLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <InternalLink target="_blank" rel="noopener noreferrer" :href="href">
+    <slot></slot>
+  </InternalLink>
+</template>
+
+<script>
+import InternalLink from "./InternalLink";
+
+export default {
+  name: "ExternalLink",
+  components: {
+    InternalLink
+  },
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
---
+++ webapp/src/components/links/InternalLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <a :target="target" :rel="rel" :href="href"><slot></slot></a>
+</template>
+
+<script>
+export default {
+  name: "InternalLink",
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    target: {
+      type: String,
+      required: true
+    },
+    rel: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+
+img {
+  display: block;
+}
+</style>
