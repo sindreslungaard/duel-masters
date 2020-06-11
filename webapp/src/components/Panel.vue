13da5f0e2b2aaa9a811ad540e26cbf487297100a
Severin Kaderli <severin@kaderli.dev>
2020-06-11 15:22:06 +0200

    Add panel component
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/Panel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="panel">
+    <header class="panel__header">
+      <h3>{{ title }}</h3>
+    </header>
+
+    <div class="panel__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.panel__content {
+  overflow: auto;
+  background: #2b2c31;
+  border-radius: 4px;
+  font-size: 14px;
+  color: var(--color-text-light);
+  padding: var(--spacing);
+  flex-grow: 1;
+}
+
+.panel__header {
+  display: flex;
+  align-items: center;
+}
+</style>
