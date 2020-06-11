4515f8a9379d97d1e435c8246b899cec04844123
Severin Kaderli <severin@kaderli.dev>
2020-06-11 17:20:48 +0200

    Add component for a loading indicator
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/LoadingIndicator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="loading-indicator">
+    Loading
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingIndicator"
+};
+</script>
+
+<style scoped>
+div::after {
+  content: ".";
+  animation: 1.5s infinite loadingDots;
+}
+
+@keyframes loadingDots {
+  from {
+    content: ".";
+  }
+
+  25% {
+    content: "..";
+  }
+
+  50% {
+    content: "...";
+  }
+
+  75% {
+    content: "....";
+  }
+
+  to {
+    content: ".";
+  }
+}
+</style>
