88af3dd8c14dc99be7969abe6d7fa927dcffe12f
Severin Kaderli <severin@kaderli.dev>
2020-06-11 16:00:30 +0200

    Add chat component
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/chat/Chat.vue
@@ -0,0 +1,103 @@
+<template>
+  <Panel title="Chat" class="chat">
+    <!-- TODO: Make a Loader component -->
+    <div v-if="isLoading">Loading</div>
+
+    <div class="chatbox">
+      <div id="messages" class="messages spaced">
+        <div class="messages-helper">
+          <div v-for="(msg, i) in messages" :key="i">
+            <Username :color="msg.color">{{ msg.username }}</Username>
+            <div class="user-messages">
+              <div v-for="(message, j) in msg.messages" :key="j">
+                <span>{{ message }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form @submit.prevent="$emit('submit')">
+        <input
+          @input="$emit('input', chatMessage)"
+          type="text"
+          v-model="chatMessage"
+          placeholder="Type to chat"
+        />
+      </form>
+    </div>
+  </Panel>
+</template>
+
+<script>
+import Username from "./Username";
+import Panel from "../Panel";
+
+export default {
+  name: "UserList",
+  components: {
+    Username,
+    Panel
+  },
+  data() {
+    return {
+      chatMessage: this.value
+    };
+  },
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      required: true
+    },
+    value: {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.chatbox {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow: hidden;
+}
+
+.chatbox input {
+  border: none;
+  border-radius: 4px;
+  width: 100%;
+  background: var(--color-background-input);
+  padding: 10px;
+  color: var(--color-text-light);
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    outline: none;
+  }
+}
+
+.user-name-container {
+  overflow: hidden;
+}
+
+.user-messages {
+  margin-left: 20px;
+  margin-top: 0px;
+  margin-bottom: 15px;
+}
+
+.user-messages > div {
+  margin: 3px 0;
+  color: #fff;
+}
+
+.messages {
+  overflow: auto;
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+</style>
