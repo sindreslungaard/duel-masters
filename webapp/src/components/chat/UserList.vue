431efaf9e419e81d17fc768e6b3177afd48a3a41
Severin Kaderli <severin@kaderli.dev>
2020-06-11 15:40:04 +0200

    Add user list component
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/chat/UserList.vue
@@ -0,0 +1,60 @@
+<template>
+  <Panel title="Online" class="user-list">
+    <!-- TODO: Make a Loader component -->
+    <div v-if="isLoading">Loading</div>
+
+    <div
+      class="user-list__category"
+      v-for="(category, index) in users"
+      :key="index"
+    >
+      <h4>{{ category.category }}</h4>
+      <ul>
+        <li v-for="(user, index) in category.users" :key="index">
+          <Username :hub="user.hub" :color="user.color">{{
+            user.username
+          }}</Username>
+        </li>
+      </ul>
+    </div>
+  </Panel>
+</template>
+
+<script>
+import Username from "./Username";
+import Panel from "../Panel";
+
+export default {
+  name: "UserList",
+  components: {
+    Username,
+    Panel
+  },
+  props: {
+    users: {
+      type: Array,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.user-list__category {
+  margin-bottom: calc(2 * var(--spacing));
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin-bottom: calc(0.5 * var(--spacing));
+}
+</style>
