0daa99dcbb4c27f9c8294dfa253875afdb785111
Severin Kaderli <severin@kaderli.dev>
2020-06-17 19:44:33 +0200

    Add ErrorDialog component
    Signed-off-by: Severin Kaderli <severin@kaderli.dev>

---
+++ webapp/src/components/dialogs/ErrorDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="error-dialog">
+    {{ message }}
+    <Button text="Reload" @click="reloadPage" />
+  </div>
+</template>
+
+<script>
+import Button from "../buttons/Button";
+
+export default {
+  name: "ErrorDialog",
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    Button
+  },
+  methods: {
+    reloadPage() {
+      window.location.reload();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  margin-top: var(--spacing);
+}
+</style>
