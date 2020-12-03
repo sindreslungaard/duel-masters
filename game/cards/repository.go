d9c139153e5819dc2676d2287635f7f47fe91fdd
Sindre Slungaard <slungaards@gmail.com>
2020-12-03 18:44:26 +0100

    New card: Plasma Chaser

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -190,4 +190,5 @@ var DM02 = map[string]match.CardConstructor{
 	"ec3213d2-6914-4091-bf86-869354a36b15": dm02.PoisonWorm,
 	"ecd61b34-85ca-4a91-b993-7a8c9976a2b5": dm02.Corile,
 	"f4a364f5-d0e9-4777-b51e-6dc6e39b803c": dm02.AquaShooter,
+	"f9b47778-c7d5-48da-aac3-1ddf8efbbbf1": dm02.PlasmaChaser,
 }
