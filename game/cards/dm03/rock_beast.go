e892009166503a0592c95629df1b9906de6bf0f5
Sindre Slungaard <slungaards@gmail.com>
2021-05-16 22:54:34 +0200

    Fix Flametropus' ability

--- game/cards/dm03/rock_beast.go
+++ game/cards/dm03/rock_beast.go
@@ -19,24 +19,28 @@ func Flametropus(c *match.Card) {
 	c.ManaCost = 4
 	c.ManaRequirement = []string{civ.Fire}
 
-	c.Use(fx.Creature, fx.When(fx.AttackingPlayer, func(card *match.Card, ctx *match.Context) {
+	c.Use(fx.Creature, fx.When(fx.Attacking, func(card *match.Card, ctx *match.Context) {
 
-		cards := fx.Select(
+		fx.Select(
 			card.Player,
 			ctx.Match,
 			card.Player,
 			match.MANAZONE,
-			"Flametropus: You may select 1 card from your mana zone that will be sent to your graveyard",
-			0,
+			"Flametropus: You may select 1 card from your mana zone that will be sent to your graveyard.",
 			1,
-			false,
-		)
+			1,
+			true,
+		).Map(func(x *match.Card) {
 
-		if len(cards) > 0 {
+			card.Player.MoveCard(x.ID, match.MANAZONE, match.GRAVEYARD)
+			ctx.Match.Chat("Server", fmt.Sprintf("%s was moved from %s manazone to their graveyard by Flametropus", x.Name, card.Player.Username()))
+
 			card.AddCondition(cnd.DoubleBreaker, nil, card.ID)
 			card.AddCondition(cnd.PowerAttacker, 3000, card.ID)
 			ctx.Match.Chat("Server", fmt.Sprintf("%s was given power attacker +3000 and double breaker until the end of the turn", card.Name))
-		}
+
+		})
+
 	}))
 
 }
