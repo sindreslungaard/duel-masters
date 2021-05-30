20f1df7b73ae71a80e5f0f0daff5317810f85df9
CristiDospra <cristi.dospra@gmail.com>
2021-05-30 19:49:51 +0300

    Fix issue with Thorny Mandra sending the card tapped to manazone

--- game/cards/dm01/tree_folk.go
+++ game/cards/dm01/tree_folk.go
@@ -56,7 +56,7 @@ func ThornyMandra(c *match.Card) {
 				creatures := match.SearchForCnd(card.Player, ctx.Match, card.Player, match.GRAVEYARD, cnd.Creature, "Thorny Mandra: Select 1 creature from your battlezone that will be sent to your manazone", 1, 1, true)
 
 				for _, creature := range creatures {
-					creature.Tapped = true
+					creature.Tapped = false
 					card.Player.MoveCard(creature.ID, match.GRAVEYARD, match.MANAZONE)
 					ctx.Match.Chat("Server", fmt.Sprintf("%s was moved from %s's graveyard to their manazone", creature.Name, card.Player.Username()))
 				}
