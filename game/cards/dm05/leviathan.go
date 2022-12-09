6104822c5a0248c23ea655ddb6af3e39e13b4964
Jyotiska <jyotiskaghosh@gmail.com>
2022-12-09 11:22:15 +0530

    improved quality

--- game/cards/dm05/leviathan.go
+++ game/cards/dm05/leviathan.go
@@ -18,34 +18,28 @@ func KingTsunami(c *match.Card) {
 	c.ManaCost = 12
 	c.ManaRequirement = []string{civ.Water}
 
-	c.Use(fx.Creature, fx.Triplebreaker, func(card *match.Card, ctx *match.Context) {
+	c.Use(fx.Creature, fx.Triplebreaker, fx.When(fx.Summoned, func(card *match.Card, ctx *match.Context) {
 
-		if event, ok := ctx.Event.(*match.CardMoved); ok {
+		myCreatures, err := card.Player.Container(match.BATTLEZONE)
+		if err != nil {
+			return
+		}
 
-			if event.CardID == card.ID && event.To == match.BATTLEZONE {
+		opponentsCreatures, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
+		if err != nil {
+			return
+		}
 
-				myCreatures, err := card.Player.Container(match.BATTLEZONE)
-				if err != nil {
-					return
-				}
+		for _, creature := range append(myCreatures, opponentsCreatures...) {
 
-				opponentsCreatures, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
-				if err != nil {
-					return
-				}
+			if creature.ID != card.ID {
 
-				for _, creature := range append(myCreatures, opponentsCreatures...) {
+				creature.Player.MoveCard(creature.ID, match.BATTLEZONE, match.HAND)
+				ctx.Match.Chat("Server", fmt.Sprintf("%s was returned to %s's hand by %s", creature.Name, creature.Player.Username(), card.Name))
 
-					if creature.ID != card.ID {
-
-						creature.Player.MoveCard(creature.ID, match.BATTLEZONE, match.HAND)
-						ctx.Match.Chat("Server", fmt.Sprintf("%s was returned to %s's hand by %s", creature.Name, creature.Player.Username(), card.Name))
-
-					}
-				}
 			}
 		}
-	})
+	}))
 
 }
 
@@ -59,76 +53,38 @@ func KingMazelan(c *match.Card) {
 	c.ManaCost = 8
 	c.ManaRequirement = []string{civ.Water}
 
-	c.Use(fx.Creature, fx.Doublebreaker, func(card *match.Card, ctx *match.Context) {
+	c.Use(fx.Creature, fx.Doublebreaker, fx.When(fx.Summoned, func(card *match.Card, ctx *match.Context) {
 
-		if event, ok := ctx.Event.(*match.CardMoved); ok {
+		cards := make(map[string][]*match.Card)
 
-			if event.CardID != card.ID || event.To != match.BATTLEZONE {
-				return
-			}
-
-			cards := make(map[string][]*match.Card)
-
-			myCards, err := card.Player.Container(match.BATTLEZONE)
-
-			if err != nil {
-				return
-			}
-
-			opponentCards, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
-
-			if err != nil {
-				return
-			}
-
-			if len(myCards) < 1 && len(opponentCards) < 1 {
-				return
-			}
-
-			cards["Your creatures"] = myCards
-			cards["Opponent's creatures"] = opponentCards
-
-			ctx.Match.NewMultipartAction(card.Player, cards, 1, 1, "Choose 1 creature in the battlezone that will be sent to its owners hands", true)
-
-			for {
-
-				action := <-card.Player.Action
-
-				if action.Cancel {
-					break
-				}
-
-				if len(action.Cards) != 1 {
-					ctx.Match.DefaultActionWarning(card.Player)
-					continue
-				}
-
-				for _, vid := range action.Cards {
-
-					ref, err := c.Player.MoveCard(vid, match.BATTLEZONE, match.HAND)
-
-					if err != nil {
-
-						ref, err := ctx.Match.Opponent(c.Player).MoveCard(vid, match.BATTLEZONE, match.HAND)
-
-						if err == nil {
-							ctx.Match.Chat("Server", fmt.Sprintf("%s was moved to %s's hand by %s", ref.Name, ctx.Match.PlayerRef(ref.Player).Socket.User.Username, card.Name))
-						}
-
-					} else {
-						ctx.Match.Chat("Server", fmt.Sprintf("%s was moved to %s's hand by %s", ref.Name, ctx.Match.PlayerRef(ref.Player).Socket.User.Username, card.Name))
-					}
-
-				}
-
-				break
-
-			}
-
-			ctx.Match.CloseAction(c.Player)
-
+		myCards, err := card.Player.Container(match.BATTLEZONE)
+		if err != nil {
+			return
 		}
 
-	})
+		opponentCards, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
+		if err != nil {
+			return
+		}
+
+		if len(myCards) < 1 && len(opponentCards) < 1 {
+			return
+		}
+
+		cards["Your creatures"] = myCards
+		cards["Opponent's creatures"] = opponentCards
+
+		fx.SelectMultipart(
+			card.Player,
+			ctx.Match,
+			cards,
+			fmt.Sprintf("%s: Choose 1 creature in the battlezone that will be sent to its owner's hand", card.Name),
+			1,
+			1,
+			true).Map(func(creature *match.Card) {
+			creature.Player.MoveCard(creature.ID, match.BATTLEZONE, match.HAND)
+			ctx.Match.Chat("Server", fmt.Sprintf("%s was returned to %s's hand by %s", creature.Name, creature.Player.Username(), card.Name))
+		})
+	}))
 
 }
--- game/cards/dm05/liquid_people.go
+++ game/cards/dm05/liquid_people.go
@@ -18,76 +18,38 @@ func AquaSurfer(c *match.Card) {
 	c.ManaCost = 6
 	c.ManaRequirement = []string{civ.Water}
 
-	c.Use(fx.Creature, fx.ShieldTrigger, func(card *match.Card, ctx *match.Context) {
+	c.Use(fx.Creature, fx.ShieldTrigger, fx.When(fx.Summoned, func(card *match.Card, ctx *match.Context) {
 
-		if event, ok := ctx.Event.(*match.CardMoved); ok {
+		cards := make(map[string][]*match.Card)
 
-			if event.CardID != card.ID || event.To != match.BATTLEZONE {
-				return
-			}
-
-			cards := make(map[string][]*match.Card)
-
-			myCards, err := card.Player.Container(match.BATTLEZONE)
-
-			if err != nil {
-				return
-			}
-
-			opponentCards, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
-
-			if err != nil {
-				return
-			}
-
-			if len(myCards) < 1 && len(opponentCards) < 1 {
-				return
-			}
-
-			cards["Your creatures"] = myCards
-			cards["Opponent's creatures"] = opponentCards
-
-			ctx.Match.NewMultipartAction(card.Player, cards, 1, 1, "Choose 1 creature in the battlezone that will be sent to its owners hands", true)
-
-			for {
-
-				action := <-card.Player.Action
-
-				if action.Cancel {
-					break
-				}
-
-				if len(action.Cards) != 1 {
-					ctx.Match.DefaultActionWarning(card.Player)
-					continue
-				}
-
-				for _, vid := range action.Cards {
-
-					ref, err := c.Player.MoveCard(vid, match.BATTLEZONE, match.HAND)
-
-					if err != nil {
-
-						ref, err := ctx.Match.Opponent(c.Player).MoveCard(vid, match.BATTLEZONE, match.HAND)
-
-						if err == nil {
-							ctx.Match.Chat("Server", fmt.Sprintf("%s was moved to %s's hand by %s", ref.Name, ctx.Match.PlayerRef(ref.Player).Socket.User.Username, card.Name))
-						}
-
-					} else {
-						ctx.Match.Chat("Server", fmt.Sprintf("%s was moved to %s's hand by %s", ref.Name, ctx.Match.PlayerRef(ref.Player).Socket.User.Username, card.Name))
-					}
-
-				}
-
-				break
-
-			}
-
-			ctx.Match.CloseAction(c.Player)
-
+		myCards, err := card.Player.Container(match.BATTLEZONE)
+		if err != nil {
+			return
 		}
 
-	})
+		opponentCards, err := ctx.Match.Opponent(card.Player).Container(match.BATTLEZONE)
+		if err != nil {
+			return
+		}
+
+		if len(myCards) < 1 && len(opponentCards) < 1 {
+			return
+		}
+
+		cards["Your creatures"] = myCards
+		cards["Opponent's creatures"] = opponentCards
+
+		fx.SelectMultipart(
+			card.Player,
+			ctx.Match,
+			cards,
+			fmt.Sprintf("%s: Choose 1 creature in the battlezone that will be sent to its owner's hand", card.Name),
+			1,
+			1,
+			true).Map(func(creature *match.Card) {
+			creature.Player.MoveCard(creature.ID, match.BATTLEZONE, match.HAND)
+			ctx.Match.Chat("Server", fmt.Sprintf("%s was returned to %s's hand by %s", creature.Name, creature.Player.Username(), card.Name))
+		})
+	}))
 
 }
