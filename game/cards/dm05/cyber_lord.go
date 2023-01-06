abd55241959a793389e74310fdf315f21c18d31f
Jyotiska <jyotiskaghosh@gmail.com>
2023-01-06 16:13:30 +0530

    Added Pokolul. Fixed Mega Detonator.

---
+++ game/cards/dm05/cyber_lord.go
@@ -0,0 +1,42 @@
+package dm05
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+	"fmt"
+)
+
+// Pokolul ...
+func Pokolul(c *match.Card) {
+
+	c.Name = "Pokolul"
+	c.Power = 2000
+	c.Civ = civ.Water
+	c.Family = family.CyberLord
+	c.ManaCost = 4
+	c.ManaRequirement = []string{civ.Water}
+
+	isAttacking := false
+
+	c.Use(fx.Creature, func(card *match.Card, ctx *match.Context) {
+
+		if event, ok := ctx.Event.(*match.AttackPlayer); ok {
+
+			if event.CardID == card.ID {
+				isAttacking = true
+			} else {
+				isAttacking = false
+			}
+
+		}
+
+		if _, ok := ctx.Event.(*match.ShieldTriggerEvent); ok && isAttacking {
+			card.Tapped = false
+			ctx.Match.Chat("Server", fmt.Sprintf("%s untapped", card.Name))
+		}
+
+	})
+
+}
