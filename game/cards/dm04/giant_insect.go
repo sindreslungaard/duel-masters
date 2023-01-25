f5f6ae77db76d1c80b852d2cb6d681bc0475184a
Sindre Slungaard <slungaards@gmail.com>
2023-01-25 22:17:53 +0100

    fix for three eyed dragonfly not choosing creature to destroy before shields/creatures to attack

--- game/cards/dm04/giant_insect.go
+++ game/cards/dm04/giant_insect.go
@@ -19,32 +19,51 @@ func ThreeEyedDragonfly(c *match.Card) {
 	c.ManaCost = 5
 	c.ManaRequirement = []string{civ.Nature}
 
-	c.Use(fx.Creature, fx.When(fx.AttackConfirmed, func(card *match.Card, ctx *match.Context) {
+	// id of the card to destroy that the user selects when attacking
+	selectedCard := ""
 
-		selected := fx.SelectFilter(
-			card.Player,
-			ctx.Match,
-			card.Player,
-			match.BATTLEZONE,
-			"Select one creature to destroy.",
-			0,
-			1,
-			false,
-			func(c *match.Card) bool { return c.ID != card.ID },
-		)
+	c.Use(
+		fx.Creature,
 
-		if len(selected) > 0 {
-			selected.Map(func(x *match.Card) {
-				ctx.Match.Destroy(x, card, match.DestroyedByMiscAbility)
-				ctx.Match.Chat("Server", fmt.Sprintf("%s was destroyed by Three-Eyed Dragonfly", x.Name))
-			})
+		fx.When(fx.Attacking, func(card *match.Card, ctx *match.Context) {
 
-			card.AddCondition(cnd.PowerAttacker, 2000, card.ID)
-			card.AddCondition(cnd.DoubleBreaker, nil, card.ID)
-			ctx.Match.Chat("Server", fmt.Sprintf("%s was given power attacker +2000 and double breaker until the end of the turn", card.Name))
+			// reset this before each attack attempt
+			selectedCard = ""
 
-		}
+			cards := fx.SelectFilter(
+				card.Player,
+				ctx.Match,
+				card.Player,
+				match.BATTLEZONE,
+				"Select one creature to destroy.",
+				0,
+				1,
+				true,
+				func(c *match.Card) bool { return c.ID != card.ID },
+			)
 
-	}))
+			if len(cards) > 0 {
+				selectedCard = cards[0].ID
+				card.AddUniqueSourceCondition(cnd.DoubleBreaker, true, card.ID)
+				card.AddCondition(cnd.PowerAmplifier, 2000, card.ID)
+			}
+		}),
+
+		fx.When(fx.AttackConfirmed, func(card *match.Card, ctx *match.Context) {
+			if selectedCard == "" {
+				return
+			}
+
+			x, err := card.Player.GetCard(selectedCard, match.BATTLEZONE)
+
+			if err != nil {
+				card.RemoveConditionBySource(card.ID)
+				return
+			}
+
+			ctx.Match.Destroy(x, card, match.DestroyedByMiscAbility)
+			ctx.Match.Chat("Server", fmt.Sprintf("%s was destroyed by Three-Eyed Dragonfly", x.Name))
+		}),
+	)
 
 }
