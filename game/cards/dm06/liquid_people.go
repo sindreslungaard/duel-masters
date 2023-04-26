1cb07d88463b253991712a3c1986b9ecc086073b
Fabian Enos <fabian.enos@gmail.com>
2023-04-26 22:24:42 +0530

    feat: Added new card "Crystal Jouster"

---
+++ game/cards/dm06/liquid_people.go
@@ -0,0 +1,21 @@
+package dm06
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+)
+
+func CrystalJouster(c *match.Card) {
+
+	c.Name = "Crystal Jouster"
+	c.Power = 7000
+	c.Civ = civ.Water
+	c.Family = family.LiquidPeople
+	c.ManaCost = 7
+	c.ManaRequirement = []string{civ.Water}
+
+	c.Use(fx.Creature, fx.Evolution, fx.Doublebreaker, fx.ReturnToHand)
+
+}
--- game/cards/repository.go
+++ game/cards/repository.go
@@ -396,4 +396,5 @@ var DM05 = map[string]match.CardConstructor{
 var DM06 = map[string]match.CardConstructor{
 	"3d32a70c-303d-46f8-862a-f7d5a3cfaafc": dm06.NeonCluster,
 	"00f39646-e15a-47a1-a085-c3522bfa5146": dm06.RaptorFish,
+	"187d4a6c-9b92-4c7c-8507-e1de185bce02": dm06.CrystalJouster,
 }
