c5e14af19d3403b25dbdebede0bd7bffe78b33f1
Zayberex <zayberex@gmail.com>
2023-07-04 22:01:36 +0200

    Added ValiantWarriorExorious and AutomatdWeaponmasterMachai

---
+++ game/cards/dm06/armorloid.go
@@ -0,0 +1,38 @@
+package dm06
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+	////"fmt"
+    "duel-masters/game/cnd"
+)
+
+func ValiantWarriorExorious(c *match.Card) {
+    
+    c.Name = "Valiant Warrior Exorious"
+    c.Power = 4000
+    c.Civ = civ.Fire
+    c.Family = family.Armorloid
+    c.ManaCost = 6
+    c.ManaRequirement = []string{civ.Fire}
+    
+    c.Use(fx.Creature, fx.AttackUntapped, fx.PowerAttacker3000)
+   
+}
+
+
+
+func AutomatedWeaponmasterMachai(c *match.Card) {
+    
+    c.Name = "Automated Weaponmaster Machai"
+    c.Power = 4000
+    c.Civ = civ.Fire
+    c.Family = family.Armorloid
+    c.ManaCost = 4
+    c.ManaRequirement = []string{civ.Fire}
+    
+    c.Use(fx.Creature, fx.ForceAttack)
+    
+}
