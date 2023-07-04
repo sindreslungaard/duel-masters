5548732bcc3b00655ad50510223cab6a715fa04c
Zayberex <zayberex@gmail.com>
2023-07-04 21:54:28 +0200

    Added CursedPincher

---
+++ game/cards/dm06/brain_jacker.go
@@ -0,0 +1,24 @@
+package dm06
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+	//"fmt"
+    //"duel-masters/game/cnd"
+)
+
+
+func CursedPincher(c *match.Card) {
+    
+    c.Name = "Cursed Pincher"
+    c.Power = 2000
+    c.Civ = civ.Darkness
+    c.Family = family.BrainJacker
+    c.ManaCost = 4
+    c.ManaRequirement = []string{civ.Darkness}
+    
+    c.Use(fx.Creature, fx.Blocker, fx.Slayer, fx.CantAttackPlayers, fx.CantAttackCreatures)
+   
+}
