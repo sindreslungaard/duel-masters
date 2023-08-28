c80e390e3982848fb42f63f637d2037bd84377d6
Zayberex <zayberex@gmail.com>
2023-08-28 19:57:57 +0200

    Added Picora's Wrench and Rikabu's Screwdriver
    +PicorasWrench
    +RikabusScrewdriver
    +updated repo

---
+++ game/cards/dm06/xenoparts.go
@@ -0,0 +1,49 @@
+package dm06
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/cnd"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+)
+
+func PicorasWrench(c *match.Card) {
+
+	c.Name = "Picora's Wrench"
+	c.Power = 2000
+	c.Civ = civ.Fire
+	c.Family = []string{family.Xenoparts}
+	c.ManaCost = 2
+	c.ManaRequirement = []string{civ.Fire}
+
+	c.Use(fx.Creature)
+}
+
+func RikabusScrewdriver(c *match.Card) {
+
+	c.Name = "Rikabu's Screwdriver"
+	c.Power = 1000
+	c.Civ = civ.Fire
+	c.Family = []string{family.Xenoparts}
+	c.ManaCost = 2
+	c.ManaRequirement = []string{civ.Fire}
+	c.TapAbility = true
+
+	c.Use(fx.Creature, fx.When(fx.TapAbility, func(card *match.Card, ctx *match.Context) {
+		fx.SelectFilter(
+			card.Player,
+			ctx.Match,
+			ctx.Match.Opponent(card.Player),
+			match.BATTLEZONE,
+			"Rikabu's Screwdriver: You may destroy one of your opponent's blockers",
+			1,
+			1,
+			true,
+			func(x *match.Card) bool { return x.HasCondition(cnd.Blocker) },
+		).Map(func(x *match.Card) {
+			ctx.Match.Destroy(x, card, match.DestroyedByMiscAbility)
+			card.Tapped = true
+		})
+	}))
+}
