f419b7096221e9f982af4f03e9d035a3a9810132
Zayberex <zayberex@gmail.com>
2023-09-07 21:42:50 +0200

    Added all demon command from dm06
    +Zorvaz, the Bonecrusher
    +Vile Mulder, Wing Of The Void
    +Daidalos, General Of Fury
    +Gnarvash, Merchant Of Blood
    +updated repo

---
+++ game/cards/dm06/demon_command.go
@@ -0,0 +1,75 @@
+package dm06
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+	"fmt"
+)
+
+func ZorvazTheBonecrusher(c *match.Card) {
+
+	c.Name = "Zorvaz, the Bonecrusher"
+	c.Power = 8000
+	c.Civ = civ.Darkness
+	c.Family = []string{family.DemonCommand}
+	c.ManaCost = 5
+	c.ManaRequirement = []string{civ.Darkness}
+
+	c.Use(fx.Creature, fx.Blocker, fx.CantAttackCreatures, fx.CantAttackPlayers, fx.Suicide)
+}
+
+func VileMulderWingOfTheVoid(c *match.Card) {
+
+	c.Name = "Vile Mulder, Wing Of The Void"
+	c.Power = 7000
+	c.Civ = civ.Darkness
+	c.Family = []string{family.DemonCommand}
+	c.ManaCost = 4
+	c.ManaRequirement = []string{civ.Darkness}
+
+	c.Use(fx.Creature, fx.CantAttackCreatures, fx.Doublebreaker, fx.Suicide)
+}
+
+func DaidalosGeneralOfFury(c *match.Card) {
+
+	c.Name = "Daidalos, General Of Fury"
+	c.Power = 11000
+	c.Civ = civ.Darkness
+	c.Family = []string{family.DemonCommand}
+	c.ManaCost = 4
+	c.ManaRequirement = []string{civ.Darkness}
+
+	c.Use(fx.Creature, fx.Doublebreaker, fx.When(fx.Attacking, func(card *match.Card, ctx *match.Context) {
+
+		creatures := match.Search(card.Player, ctx.Match, card.Player, match.BATTLEZONE, "Daidalos, General Of Fury: Select 1 creature from your battlezone that will be sent to your graveyard", 1, 1, false)
+
+		for _, creature := range creatures {
+			ctx.Match.Destroy(creature, card, match.DestroyedByMiscAbility)
+			if creature == card {
+				ctx.InterruptFlow()
+			}
+		}
+
+	}))
+}
+
+func GnarvashMerchantOfBlood(c *match.Card) {
+
+	c.Name = "Gnarvash, Merchant Of Blood"
+	c.Power = 8000
+	c.Civ = civ.Darkness
+	c.Family = []string{family.DemonCommand}
+	c.ManaCost = 6
+	c.ManaRequirement = []string{civ.Darkness}
+
+	c.Use(fx.Creature, fx.Doublebreaker, fx.When(fx.EndOfMyTurn, func(card *match.Card, ctx *match.Context) {
+
+		if len(fx.Find(card.Player, match.BATTLEZONE)) == 1 {
+			card.Player.MoveCard(card.ID, match.BATTLEZONE, match.GRAVEYARD)
+			ctx.Match.Chat("Server", fmt.Sprintf("%s couldn't bear living alone and died", c.Name))
+		}
+
+	}))
+}
