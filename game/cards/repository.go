b30d95ac501fbf5a1c637cecac3ba130a6d94876
Zayberex <zayberex@gmail.com>
2023-09-09 14:57:27 +0200

    Added Madrillon Fish
    +MadrillonFish
    +updated repo

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -421,4 +421,5 @@ var DM06 = map[string]match.CardConstructor{
 	"41b82092-4d34-4b3c-934a-d7bd4fca654a": dm06.GarabonTheGlider,
 	"1b83ff2c-3095-40d5-bd76-e1566836a8ea": dm06.RikabusScrewdriver,
 	"27c58f10-82ef-47ca-8a69-1cfa2057743d": dm06.PicorasWrench,
+	"0743092b-fd5e-41ab-8e38-d6664c1741cd": dm06.MadrillonFish,
 }
