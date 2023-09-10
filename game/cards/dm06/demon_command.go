1d37e92f5d689809e341f916ede79d7b1d9bb410
Zayberex <zayberex@gmail.com>
2023-09-10 20:20:36 +0200

    Update demon_command.go

--- game/cards/dm06/demon_command.go
+++ game/cards/dm06/demon_command.go
@@ -41,7 +41,7 @@ func DaidalosGeneralOfFury(c *match.Card) {
 	c.ManaCost = 4
 	c.ManaRequirement = []string{civ.Darkness}
 
-	c.Use(fx.Creature, fx.Doublebreaker, fx.When(fx.Attacking, func(card *match.Card, ctx *match.Context) {
+	c.Use(fx.Creature, fx.Doublebreaker, fx.When(fx.AttackConfirmed, func(card *match.Card, ctx *match.Context) {
 
 		creatures := match.Search(card.Player, ctx.Match, card.Player, match.BATTLEZONE, "Daidalos, General Of Fury: Select 1 creature from your battlezone that will be sent to your graveyard", 1, 1, false)
 
