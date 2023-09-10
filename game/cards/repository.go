5e72b23c536fbd0ccdae4749a98262882a6c7efd
Zayberex <zayberex@gmail.com>
2023-09-10 18:41:57 +0200

    Added Kanesill, the Explorer and Telitol, the Explorer
    +KanesillTheExplorer
    +TelitolTheExplorer
    +updated repo

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -421,4 +421,6 @@ var DM06 = map[string]match.CardConstructor{
 	"41b82092-4d34-4b3c-934a-d7bd4fca654a": dm06.GarabonTheGlider,
 	"1b83ff2c-3095-40d5-bd76-e1566836a8ea": dm06.RikabusScrewdriver,
 	"27c58f10-82ef-47ca-8a69-1cfa2057743d": dm06.PicorasWrench,
+	"6bfebd3d-64ff-4321-bb0b-e994ca8f811e": dm06.TelitolTheExplorer,
+	"da51845c-4a6b-4c36-9c7d-fbb654ba2aa2": dm06.KanesillTheExplorer,
 }
