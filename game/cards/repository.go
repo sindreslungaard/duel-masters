e79f13cba14fed81994fb37ab022e10401163787
Zayberex <zayberex@gmail.com>
2023-09-14 09:06:59 +0200

    Added Q-tronic Hypermind
    +QTronicHypermind
    +updated survivors families
    +updated repo

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -421,4 +421,5 @@ var DM06 = map[string]match.CardConstructor{
 	"41b82092-4d34-4b3c-934a-d7bd4fca654a": dm06.GarabonTheGlider,
 	"1b83ff2c-3095-40d5-bd76-e1566836a8ea": dm06.RikabusScrewdriver,
 	"27c58f10-82ef-47ca-8a69-1cfa2057743d": dm06.PicorasWrench,
+	"59da70cc-e655-4e70-b467-8024dc7c472e": dm06.QTronicHypermind,
 }
