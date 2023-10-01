63f28aafc3325b13dfe147fb97ae7f3c290dbe9c
sindreslungaard <slungaards@gmail.com>
2023-10-01 21:40:31 +0200

    fix db name conversions for duel history

--- db/duels_model.go
+++ db/duels_model.go
@@ -3,14 +3,14 @@ package db
 type DuelResolution string
 
 type Duel struct {
-	UID       string `json:"uid"`
-	Host      string `json:"p1"`
-	HostDeck  string `json:"p1deck"`
-	Guest     string `json:"p2"`
-	GuestDeck string `json:"p2deck"`
-	Started   int64  `json:"startedAt"`
-	Ended     int64  `json:"endedAt"`
-	Winner    string `json:"winner"`
+	UID       string `bson:"uid"`
+	Host      string `bson:"p1"`
+	HostDeck  string `bson:"p1deck"`
+	Guest     string `bson:"p2"`
+	GuestDeck string `bson:"p2deck"`
+	Started   int64  `bson:"startedAt"`
+	Ended     int64  `bson:"endedAt"`
+	Winner    string `bson:"winner"`
 }
 
 var Duels = conn().Collection("duels")
