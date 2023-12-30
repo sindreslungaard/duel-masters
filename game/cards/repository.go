0246c3eeebe906e29d2bf1e1d91f1256f0d3f8be
Zayberex <zayberex@gmail.com>
2023-12-30 17:29:19 +0100

    Added Feather Horn and Paradise Horn
    +FeatherHornTheTracker
    +ParadiseHorn
    +updated repo

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -477,4 +477,6 @@ var DM06 = map[string]match.CardConstructor{
 	"7c33ae39-5079-471b-982c-98da274c9892": dm06.RazorpineTree,
 	"cc7916d4-9f3a-4993-8283-134f3bba105c": dm06.CliffcrushGiant,
 	"e0538282-a50e-4097-840c-80f262ce0416": dm06.CantankerousGiant,
+	"3031a721-6725-42f3-a694-08e931eb9fc6": dm06.FeatherHornTheTracker,
+	"8fdf84b5-7fa2-4caa-820e-5c045fc51156": dm06.ParadiseHorn,
 }
