1288988d67e2dca6e6a0d031dab8213558efec41
Zayberex <zayberex@gmail.com>
2024-02-21 18:36:56 +0100

    Added Aeropica and Zepimeteus
    +Aeropica
    +Zepimeteus
    +updated repo

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -477,4 +477,6 @@ var DM06 = map[string]match.CardConstructor{
 	"7c33ae39-5079-471b-982c-98da274c9892": dm06.RazorpineTree,
 	"cc7916d4-9f3a-4993-8283-134f3bba105c": dm06.CliffcrushGiant,
 	"e0538282-a50e-4097-840c-80f262ce0416": dm06.CantankerousGiant,
+	"eb15fb12-f761-44d7-a91f-4f85a9d27814": dm06.Zepimeteus,
+	"ec0b5fbc-3a94-4f45-80f8-063daacb62e0": dm06.Aeropica,
 }
