13a4cc6679e485beb0042f0678a2ed6b38358734
Daniel <>
2024-07-16 14:05:16 +0800

    Add Torchclencher dm06

--- game/cards/dm06/dragonoid.go
+++ game/cards/dm06/dragonoid.go
@@ -26,3 +26,27 @@ func PyrofighterMagnus(c *match.Card) {
 		ctx.Match.Chat("Server", fmt.Sprintf("%s was returned to the %s's hand", c.Name, c.Player.Username()))
 	}))
 }
+
+func Torchclencher(c *match.Card) {
+
+	c.Name = "Torchclencher"
+	c.Power = 2000
+	c.Civ = civ.Fire
+	c.Family = []string{family.Dragonoid}
+	c.ManaCost = 3
+	c.ManaRequirement = []string{civ.Fire}
+
+	c.Use(fx.Creature)
+
+	c.PowerModifier = func(m *match.Match, attacking bool) int {
+
+		power := 0
+
+		if attacking && match.ContainerHas(c.Player, match.BATTLEZONE, func(x *match.Card) bool { return x != c && x.Civ == civ.Fire }) {
+			power += 3000
+		}
+
+		return power
+	}
+
+}
--- game/cards/repository.go
+++ game/cards/repository.go
@@ -488,4 +488,5 @@ var DM06 = map[string]match.CardConstructor{
 	"76da3804-df25-4773-ba2f-ea17bab89f2d": dm06.CometMissile,
 	"5c424a0f-5bbd-41cd-9279-2b408f7e5935": dm06.CrisisBoulder,
 	"124dc6bb-a6c3-4771-91a5-9cd2c2b198e7": dm06.FactoryShellQ,
+	"08a6f16d-36bc-48a9-a704-122875759618": dm06.Torchclencher,
 }
