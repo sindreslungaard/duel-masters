b484fd30da3452eab57746af492fd1d2eada281b
Daniel S <4135641+DanieloV@users.noreply.github.com>
2024-08-04 18:23:12 +0800

    Add Armored Scout Gestuchar (dm06)

--- game/cards/dm06/armorloid.go
+++ game/cards/dm06/armorloid.go
@@ -2,6 +2,7 @@ package dm06
 
 import (
 	"duel-masters/game/civ"
+	"duel-masters/game/cnd"
 	"duel-masters/game/family"
 	"duel-masters/game/fx"
 	"duel-masters/game/match"
@@ -30,5 +31,45 @@ func AutomatedWeaponmasterMachai(c *match.Card) {
 	c.ManaRequirement = []string{civ.Fire}
 
 	c.Use(fx.Creature, fx.ForceAttack)
+
+}
+
+func ArmoredScoutGestuchar(c *match.Card) {
+
+	c.Name = "Armored Scout Gestuchar"
+	c.Power = 4000
+	c.Civ = civ.Fire
+	c.Family = []string{family.Armorloid}
+	c.ManaCost = 5
+	c.ManaRequirement = []string{civ.Fire}
+
+	c.Use(fx.Creature, fx.When(fx.Summoned, func(card *match.Card, ctx *match.Context) {
+
+		ctx.Match.ApplyPersistentEffect(func(ctx2 *match.Context, exit func()) {
+			// Remove the conditions and exit the persistent effect when the card is no longer in the battle zone
+			if card.Zone != match.BATTLEZONE {
+				card.RemoveConditionBySource(card.ID)
+				exit()
+				return
+			}
+
+			// Whenever a card is moved while the persistent effect is active, remove the conditions
+			// and add them back only if there are no other fire creatures in the battle zone
+			if _, ok := ctx2.Event.(*match.CardMoved); ok {
+				card.RemoveConditionBySource(card.ID)
+
+				fireCreatures := fx.FindFilter(
+					c.Player,
+					match.BATTLEZONE,
+					func(card *match.Card) bool { return card.Civ == civ.Fire },
+				)
+
+				if len(fireCreatures) == 1 {
+					card.AddUniqueSourceCondition(cnd.PowerAttacker, 3000, c.ID)
+					card.AddUniqueSourceCondition(cnd.DoubleBreaker, true, c.ID)
+				}
+			}
+		})
+	}))
 
 }
