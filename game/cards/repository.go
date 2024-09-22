5173b84dbd0522288362e0ca5a38da891146935e
Daniel S <4135641+DanieloV@users.noreply.github.com>
2024-09-22 12:54:54 +0300

    add Popple, Flowerpetal Dancer dm07

--- game/cards/repository.go
+++ game/cards/repository.go
@@ -577,7 +577,7 @@ var DM07 = map[string]match.CardConstructor{
 	"30272397-4b70-4b40-b7ee-8399ebe099d6": nil,
 	"e6c53caf-9923-4496-a817-2727ea17bead": dm07.LaunchLocust,
 	"8de000dc-4cd9-4a6b-bccc-acfae89211ef": nil,
-	"bb3a50dd-049d-488d-b89c-779dcf29b82e": nil,
+	"bb3a50dd-049d-488d-b89c-779dcf29b82e": dm07.PoppleFlowerpetalDancer,
 	"3e3b883e-721c-42b9-8181-52e514695c7f": nil,
 	"a5cba5aa-271f-43b0-8350-ca94c5cdfe9a": nil,
 	"ced03efd-90c8-4303-83b4-16d2d1198aaf": nil,
