f176337f0f31b2758dfbeb8a78a483342c1ae326
Daniel S <4135641+DanieloV@users.noreply.github.com>
2024-11-04 14:24:14 +0200

    Add Miracle Portal dm07 + Refactor can't attack logic

--- game/cards/dm05/chimera.go
+++ game/cards/dm05/chimera.go
@@ -1,8 +1,6 @@
 package dm05
 
 import (
-	"fmt"
-
 	"duel-masters/game/civ"
 	"duel-masters/game/cnd"
 	"duel-masters/game/family"
@@ -19,69 +17,7 @@ func Gigazoul(c *match.Card) {
 	c.ManaCost = 3
 	c.ManaRequirement = []string{civ.Darkness}
 
-	c.Use(fx.Creature, func(card *match.Card, ctx *match.Context) {
-
-		if _, ok := ctx.Event.(*match.UntapStep); ok {
-
-			opponentShields := fx.Find(ctx.Match.Opponent(card.Player), match.SHIELDZONE)
-			n := len(opponentShields)
-
-			if n == 0 {
-				card.AddCondition(cnd.CantAttackPlayers, true, card.ID)
-				card.AddCondition(cnd.CantAttackCreatures, true, card.ID)
-			} else {
-				card.RemoveCondition(cnd.CantAttackPlayers)
-				card.RemoveCondition(cnd.CantAttackCreatures)
-			}
-
-		}
-
-		if event, ok := ctx.Event.(*match.CardMoved); ok {
-
-			if event.From == match.SHIELDZONE || event.To == match.SHIELDZONE {
-
-				opponentShields := fx.Find(ctx.Match.Opponent(card.Player), match.SHIELDZONE)
-				n := len(opponentShields)
-
-				if n == 0 {
-					card.AddCondition(cnd.CantAttackPlayers, true, card.ID)
-					card.AddCondition(cnd.CantAttackCreatures, true, card.ID)
-				} else {
-					card.RemoveCondition(cnd.CantAttackPlayers)
-					card.RemoveCondition(cnd.CantAttackCreatures)
-				}
-
-			}
-
-		}
-
-		if event, ok := ctx.Event.(*match.AttackPlayer); ok {
-
-			// Is this event for me or someone else?
-			if event.CardID != card.ID || !card.HasCondition(cnd.CantAttackPlayers) {
-				return
-			}
-
-			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s can't attack players", card.Name))
-
-			ctx.InterruptFlow()
-
-		}
-
-		if event, ok := ctx.Event.(*match.AttackCreature); ok {
-
-			// Is this event for me or someone else?
-			if event.CardID != card.ID || !card.HasCondition(cnd.CantAttackCreatures) {
-				return
-			}
-
-			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s can't attack creatures", card.Name))
-
-			ctx.InterruptFlow()
-
-		}
-
-	})
+	c.Use(fx.Creature, fx.CantAttackIfOpHasNoShields)
 
 }
 
--- game/cards/dm05/demon_command.go
+++ game/cards/dm05/demon_command.go
@@ -2,11 +2,9 @@ package dm05
 
 import (
 	"duel-masters/game/civ"
-	"duel-masters/game/cnd"
 	"duel-masters/game/family"
 	"duel-masters/game/fx"
 	"duel-masters/game/match"
-	"fmt"
 )
 
 // DeathCruzerTheAnnihilator ...
@@ -43,68 +41,6 @@ func VashunaSwordDancer(c *match.Card) {
 	c.ManaCost = 5
 	c.ManaRequirement = []string{civ.Darkness}
 
-	c.Use(fx.Creature, fx.Doublebreaker, func(card *match.Card, ctx *match.Context) {
-
-		if _, ok := ctx.Event.(*match.UntapStep); ok {
-
-			opponentShields := fx.Find(ctx.Match.Opponent(card.Player), match.SHIELDZONE)
-			n := len(opponentShields)
-
-			if n == 0 {
-				card.AddCondition(cnd.CantAttackPlayers, true, card.ID)
-				card.AddCondition(cnd.CantAttackCreatures, true, card.ID)
-			} else {
-				card.RemoveCondition(cnd.CantAttackPlayers)
-				card.RemoveCondition(cnd.CantAttackCreatures)
-			}
-
-		}
-
-		if event, ok := ctx.Event.(*match.CardMoved); ok {
-
-			if event.From == match.SHIELDZONE || event.To == match.SHIELDZONE {
-
-				opponentShields := fx.Find(ctx.Match.Opponent(card.Player), match.SHIELDZONE)
-				n := len(opponentShields)
-
-				if n == 0 {
-					card.AddCondition(cnd.CantAttackPlayers, true, card.ID)
-					card.AddCondition(cnd.CantAttackCreatures, true, card.ID)
-				} else {
-					card.RemoveCondition(cnd.CantAttackPlayers)
-					card.RemoveCondition(cnd.CantAttackCreatures)
-				}
-
-			}
-
-		}
-
-		if event, ok := ctx.Event.(*match.AttackPlayer); ok {
-
-			// Is this event for me or someone else?
-			if event.CardID != card.ID || !card.HasCondition(cnd.CantAttackPlayers) {
-				return
-			}
-
-			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s can't attack players", card.Name))
-
-			ctx.InterruptFlow()
-
-		}
-
-		if event, ok := ctx.Event.(*match.AttackCreature); ok {
-
-			// Is this event for me or someone else?
-			if event.CardID != card.ID || !card.HasCondition(cnd.CantAttackCreatures) {
-				return
-			}
-
-			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s can't attack creatures", card.Name))
-
-			ctx.InterruptFlow()
-
-		}
-
-	})
+	c.Use(fx.Creature, fx.Doublebreaker, fx.CantAttackIfOpHasNoShields)
 
 }
