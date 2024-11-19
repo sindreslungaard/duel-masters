fbd136f7a2c82e198049c66d984cdc5d3a1ae846
iitsg <99927452+iitsg@users.noreply.github.com>
2024-11-19 11:34:56 +0100

    adds stats to user model

--- db/user_model.go
+++ db/user_model.go
@@ -7,16 +7,20 @@ type UserSession struct {
 }
 
 type User struct {
-	UID         string        `json:"uid"`
-	Permissions []string      `json:"permissions"`
-	Username    string        `json:"username"`
-	Password    string        `json:"-"`
-	Email       string        `json:"email"`
-	Color       string        `json:"color"`
-	Playmat     string        `json:"playmat"`
-	Sessions    []UserSession `json:"-"`
-	MutedUsers  []string      `json:"muted_users"`
-	Chatblocked bool          `json:"-" bson:"chat_blocked"`
+	UID              string        `json:"uid"`
+	Permissions      []string      `json:"permissions"`
+	Username         string        `json:"username"`
+	Password         string        `json:"-"`
+	Email            string        `json:"email"`
+	Color            string        `json:"color"`
+	Playmat          string        `json:"playmat"`
+	Sessions         []UserSession `json:"-"`
+	MutedUsers       []string      `json:"muted_users"`
+	Chatblocked      bool          `json:"-" bson:"chat_blocked"`
+	TotalGamesPlayed int           `json:"total_games_played" bson:"total_games_played"`
+	GamesWon         int           `json:"games_won" bson:"games_won"`
+	GamesLost        int           `json:"games_lost" bson:"games_lost"`
+	WinRate          float64       `json:"win_rate" bson:"win_rate"`
 }
 
 var Users = conn().Collection("users")
