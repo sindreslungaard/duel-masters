ac649af387b04a6340f275583fd7b33e721d2acf
Jyotiska <jyotiskaghosh@gmail.com>
2024-11-28 19:37:26 +0530

    Reimplement Speed Attacker as a Condition. Implement Turbo Rush. Add Jagalzor.

--- game/fx/creature.go
+++ game/fx/creature.go
@@ -162,7 +162,7 @@ func Creature(card *match.Card, ctx *match.Context) {
 			return
 		}
 
-		if card.HasCondition(cnd.SummoningSickness) {
+		if hasSummoningSickness(card) {
 			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s cannot attack this turn as it has summoning sickness", card.Name))
 			ctx.InterruptFlow()
 			return
@@ -279,7 +279,7 @@ func Creature(card *match.Card, ctx *match.Context) {
 			return
 		}
 
-		if card.HasCondition(cnd.SummoningSickness) {
+		if hasSummoningSickness(card) {
 			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s cannot attack this turn as it has summoning sickness", card.Name))
 			ctx.InterruptFlow()
 			return
@@ -517,7 +517,7 @@ func Creature(card *match.Card, ctx *match.Context) {
 			return
 		}
 
-		if card.HasCondition(cnd.SummoningSickness) {
+		if hasSummoningSickness(card) {
 			ctx.Match.WarnPlayer(card.Player, fmt.Sprintf("%s can't use tap ability because it has summoning sickness", card.Name))
 			ctx.InterruptFlow()
 			return
@@ -700,4 +700,8 @@ func stealthActive(card *match.Card, ctx *match.Context) bool {
 	}
 
 	return false
+}
+
+func hasSummoningSickness(card *match.Card) bool {
+	return card.HasCondition(cnd.SummoningSickness) && !card.HasCondition(cnd.SpeedAttacker)
 }
