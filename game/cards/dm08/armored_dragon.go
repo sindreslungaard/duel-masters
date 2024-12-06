b2d7e2bbb9c80ec43badaa5c6d053e040d7f51ce
Jyotiska <jyotiskaghosh@gmail.com>
2024-12-07 00:18:23 +0530

    Implement Dragon Evolution. Add Bajula and Abzo Dolba.

---
+++ game/cards/dm08/armored_dragon.go
@@ -0,0 +1,22 @@
+package dm08
+
+import (
+	"duel-masters/game/civ"
+	"duel-masters/game/family"
+	"duel-masters/game/fx"
+	"duel-masters/game/match"
+)
+
+// UberdragonBajula ...
+func UberdragonBajula(c *match.Card) {
+
+	c.Name = "\u00c3\u0153berdragon Bajula"
+	c.Power = 13000
+	c.Civ = civ.Fire
+	c.Family = []string{family.ArmoredDragon}
+	c.ManaCost = 7
+	c.ManaRequirement = []string{civ.Fire}
+
+	c.Use(fx.Creature, fx.DragonEvolution, fx.Triplebreaker, fx.WheneverThisAttacks(fx.ManaBurnX(2)))
+
+}
--- game/cards/dm08/zombie_dragon.go
+++ game/cards/dm08/zombie_dragon.go
@@ -2,6 +2,7 @@ package dm08
 
 import (
 	"duel-masters/game/civ"
+	"duel-masters/game/cnd"
 	"duel-masters/game/family"
 	"duel-masters/game/fx"
 	"duel-masters/game/match"
@@ -44,4 +45,26 @@ func NecrodragonGalbazeek(c *match.Card) {
 			ctx.Match.MoveCard(x, match.GRAVEYARD, card)
 		})
 	}))
+}
+
+// SuperNecrodragonAbzoDolba ...
+func SuperNecrodragonAbzoDolba(c *match.Card) {
+
+	c.Name = "Super Necrodragon Abzo Dolba"
+	c.Power = 11000
+	c.Civ = civ.Darkness
+	c.Family = []string{family.ZombieDragon}
+	c.ManaCost = 6
+	c.ManaRequirement = []string{civ.Darkness}
+
+	c.PowerModifier = func(m *match.Match, attacking bool) int {
+
+		creaturesInGrave := fx.FindFilter(c.Player, match.GRAVEYARD, func(x *match.Card) bool { return x.HasCondition(cnd.Creature) })
+
+		return len(creaturesInGrave) * 2000
+
+	}
+
+	c.Use(fx.Creature, fx.DragonEvolution, fx.Triplebreaker)
+
 }
