14211e7a613c303c68b583d51846eb7c96ccb0c9
Dragos Ionita <dragos.ionita@lobyco.com>
2025-07-30 14:49:50 +0300

    x

--- game/fx/charger.go
+++ game/fx/charger.go
@@ -8,8 +8,8 @@ import (
 func Charger(card *match.Card, ctx *match.Context) {
 	// Charger
 	// After you cast this spell, put it into your mana zone instead of your graveyard.
-	// if fx.SpellCast(card, ctx) {
 	if event, ok := ctx.Event.(*match.SpellCast); ok && event.CardID == card.ID {
+		// if fx.SpellCast(card, ctx) {
 		card.Player.MoveCard(card.ID, match.HAND, match.MANAZONE, card.ID)
 		card.Tapped = false
 		ctx.Match.ReportActionInChat(card.Player, fmt.Sprintf("%s was put in the mana zone", card.Name))
