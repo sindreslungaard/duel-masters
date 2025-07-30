2795f871023a77071f2480a974ce56732e8e9367
Dragos Ionita <dragos.ionita@lobyco.com>
2025-07-30 14:50:21 +0300

    xx

--- game/fx/charger.go
+++ game/fx/charger.go
@@ -6,6 +6,7 @@ import (
 )
 
 func Charger(card *match.Card, ctx *match.Context) {
+
 	// Charger
 	// After you cast this spell, put it into your mana zone instead of your graveyard.
 	if event, ok := ctx.Event.(*match.SpellCast); ok && event.CardID == card.ID {
