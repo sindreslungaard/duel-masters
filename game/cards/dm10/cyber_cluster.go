8fbb3bc0b8f9299cbbf63aa6f5cf79b94fc6cd02
Dragos Ionita <gr99drg@gmail.com>
2025-10-03 00:47:15 +0300

    Tide Patroller

--- game/cards/dm10/cyber_cluster.go
+++ game/cards/dm10/cyber_cluster.go
@@ -20,3 +20,17 @@ func BatteryCluster(c *match.Card) {
 	c.Use(fx.Creature, fx.Blocker(), fx.CantAttackCreatures, fx.CantAttackPlayers)
 
 }
+
+// TorpedoCluster ...
+func TorpedoCluster(c *match.Card) {
+
+	c.Name = "Torpedo Cluster"
+	c.Power = 3000
+	c.Civ = civ.Water
+	c.Family = []string{family.CyberCluster}
+	c.ManaCost = 3
+	c.ManaRequirement = []string{civ.Water}
+
+	c.Use(fx.Creature, fx.When(fx.Summoned, fx.ReturnMyCardFromMZToHand))
+
+}
