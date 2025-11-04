71551da2a07f7704d1d10da58fefac1711f5f31a
Dragos Ionita <gr99drg@gmail.com>
2025-11-04 18:54:47 +0200

    Fix Upheaval

--- game/cards/dm10/spells.go
+++ game/cards/dm10/spells.go
@@ -16,30 +16,30 @@ func Upheaval(c *match.Card) {
 	c.ManaRequirement = []string{civ.Darkness}
 
 	c.Use(fx.Spell, fx.ShieldTrigger, fx.When(fx.SpellCast, func(card *match.Card, ctx *match.Context) {
-		myManaCards, err1 := card.Player.Container(match.MANAZONE)
-		myHandCards, err2 := card.Player.Container(match.HAND)
-		myOppManaCards, err3 := ctx.Match.Opponent(card.Player).Container(match.MANAZONE)
-		myOppHandCards, err4 := ctx.Match.Opponent(card.Player).Container(match.HAND)
+		myManaCards := fx.Find(card.Player, match.MANAZONE)
+		myHandCards := fx.FindFilter(card.Player, match.HAND, func(x *match.Card) bool { return x.ID != card.ID })
+		myOppManaCards := fx.Find(ctx.Match.Opponent(card.Player), match.MANAZONE)
+		myOppHandCards := fx.Find(ctx.Match.Opponent(card.Player), match.HAND)
 
-		if err1 == nil && err2 == nil && err3 == nil && err4 == nil {
-			for _, x := range myManaCards {
-				ctx.Match.MoveCard(x, match.HAND, card, true)
-			}
-
-			for _, x := range myHandCards {
-				ctx.Match.MoveCard(x, match.MANAZONE, card, true)
-			}
-
-			for _, x := range myOppManaCards {
-				ctx.Match.MoveCard(x, match.HAND, card, true)
-			}
-
-			for _, x := range myOppHandCards {
-				ctx.Match.MoveCard(x, match.MANAZONE, card, true)
-			}
-
-			ctx.Match.ReportActionInChat(card.Player, fmt.Sprintf("%s's effect: Both players moved their mana cards to their hand, and at the same time, their hand cards to their mana zone.", card.Name))
+		for _, x := range myManaCards {
+			ctx.Match.MoveCard(x, match.HAND, card, true)
 		}
+
+		for _, x := range myHandCards {
+			ctx.Match.MoveCard(x, match.MANAZONE, card, true)
+			x.Tapped = true
+		}
+
+		for _, x := range myOppManaCards {
+			ctx.Match.MoveCard(x, match.HAND, card, true)
+		}
+
+		for _, x := range myOppHandCards {
+			ctx.Match.MoveCard(x, match.MANAZONE, card, true)
+			x.Tapped = true
+		}
+
+		ctx.Match.ReportActionInChat(card.Player, fmt.Sprintf("%s's effect: Both players moved their mana cards to their hand, and at the same time, their hand cards to their mana zone.", card.Name))
 	}))
 
 }
