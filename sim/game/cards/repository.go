afaf05528552d4652ab6388a245d8011c37eadfc
Dragos Ionita <gr99drg@gmail.com>
2026-08-15 01:25:56 +0300

    "Cosmic Darts", DM-12 missing card.

--- sim/game/cards/repository.go
+++ sim/game/cards/repository.go
@@ -957,6 +957,7 @@ var DM12 = map[string]match.CardConstructor{
 	"02f11a2f-ff50-4e0d-80bd-2995be9d3dd8": dm12.WingeyeMoth,
 	"05c5496d-e5fa-4691-8542-2d6c6919f402": dm12.UlarusPunishmentElemental,
 	"76310adb-f7c2-4545-8b71-2332b36fbb83": dm12.Gigavrand,
+	"0fe53ab5-bef9-4bbd-bb05-c94ccb9b1342": dm12.CosmicDarts,
 }
 
 // Promo is a map with all the card id's in the game and corresponding CardConstructor for promotional exclusive cards
